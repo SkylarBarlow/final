1a6a10ce7720c83f4ffe706f023128f15bb7cbaf
SkylarBarlow <47986644+SkylarBarlow@users.noreply.github.com>
2019-04-26 10:46:28 -0400

    Add files via upload

---
+++ code.js
@@ -0,0 +1,17 @@
+ $(document).ready(function() {
+      $('.toggleNav').click(function() {
+        $('#flex-nav ul').toggleClass('open');
+      });
+  $('.call').click(function() {
+   $('.phone').slideToggle('slow');
+  });
+ $('.button').click(
+    function(){
+     $(this).removeClass('shadow')
+   });
+  $('.button').mouseleave(
+     function(){
+     $(this).addClass('shadow');
+     });
+    });
+
